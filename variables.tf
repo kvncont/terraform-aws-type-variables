381975fe51541ba08a845052316a6f2db0597284
Kevin Contreras Rodríguez <kvncont@gmail.com>
2025-03-23 18:50:07 -0600

    test

--- variables.tf
+++ variables.tf
@@ -7,3 +7,27 @@ variable "assume_role_arn" {
   type = string
   description = "The ARN of the role to assume by Terraform"
 }
+
+variable "my_list" {
+  type = list(string)
+  description = "Tests list"
+}
+
+variable "my_map" {
+  type = map(string)
+  description = "Map string"
+}
+
+variable "my_list_objects" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+}
+
+variable "my_string_default" {
+  type = string
+  description = "test string default"
+  default = "default value"
+}
